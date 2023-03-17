5ea0648d219ef6d22b67c626c4926baa0cdf6724
Dennis <dennis@test.com>
2023-03-17 00:08:27 -0500

    MyTheme.js updated

--- src/MyTheme.js
+++ src/MyTheme.js
@@ -1,26 +1,32 @@
 import { createTheme } from '@mui/material/styles';
-import { light } from '@mui/material/styles/createPalette';
 
 /* palatte */
 /* #2b2d42, #8d99ae, #edf2f4, #ef233c, #d90429 */
 
-
+export const colors = {
+  "navyBlue": "#2b2d42",
+  "grey": "#8d99ae",
+  "lightGrey": "#edf2f4",
+  "lightRed": "#ef233c",
+  "red": "#d90429",
+}
 
 export const MyTheme = createTheme({
   palette: {
     primary: {
-      main: '#2b2d42',
+      main: colors.navyBlue,
       light: '#54566d',
       dark: '#02021c',
     },
     secondary: {
-      main: '#8d99ae',
-      light: '#8d99ae',
-      dark: '#8d99ae',
+      main: colors.grey,
+      light: colors.grey,
+      dark: colors.grey,
     },
     text: {
-      primary: '#2b2d42',
-      secondary: "#8d99ae"
+      primary: colors.navyBlue,
+      secondary: colors.grey
     }
   }
 })
+
