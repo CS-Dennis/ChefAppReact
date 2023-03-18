f53d8640a730b66b6bab16e2aecff978f1b9d273
Dennis <dennis@test.com>
2023-03-18 13:58:39 -0500

    added validate int and float methods in utils.js

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,3 +1,14 @@
 export const trimString = (input) => {
   return input.trim();
 }
+
+export const validateInt = (integer) => {
+  const conversion = Number(integer);
+  return Number.isInteger(conversion);
+}
+
+// validate if it's float or int
+export const validateFloat = (float) => {
+  const conversion = Number(float);
+  return !Number.isNaN(conversion);
+}
