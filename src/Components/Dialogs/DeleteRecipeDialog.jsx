35088ba6593d1eda7a5706c38d43aa23520a73cd
Dennis <dennis@test.com>
2023-03-23 22:55:41 -0500

    DeleteRecipeDialog minor update

--- src/Components/Dialogs/DeleteRecipeDialog.jsx
+++ src/Components/Dialogs/DeleteRecipeDialog.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Dialog, DialogTitle } from '@mui/material'
 import React from 'react'
+import { colors } from '../../MyTheme';
 
 export default function DeleteRecipeDialog({ showDeleteRecipeDialog, setShowDeleteRecipeDialog }) {
   const deleteRecipe = () => {
@@ -15,7 +16,7 @@ export default function DeleteRecipeDialog({ showDeleteRecipeDialog, setShowDele
 
         <Box sx={{ display: "flex" }} >
           <Box sx={{ display: 'flex', padding: '10px', width: '100%', justifyContent: 'space-evenly', }}>
-            <Button variant='outlined' sx={{ color: 'red', width: '30%' }} onClick={deleteRecipe}>Yes</Button>
+            <Button variant='outlined' sx={{ color: colors.red, width: '30%' }} onClick={deleteRecipe}>Yes</Button>
             <Button variant='outlined' sx={{ width: '30%' }} onClick={() => setShowDeleteRecipeDialog(false)}>No</Button>
           </Box>
         </Box>
