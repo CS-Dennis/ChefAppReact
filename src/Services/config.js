2985f6b7f88ae1eac3a2bffd4d013c504806c57b
Dennis <dennis@test.com>
2023-03-27 23:44:55 -0500

    config.js update

--- src/Services/config.js
+++ src/Services/config.js
@@ -1,2 +1,12 @@
-export const serverURL = "http://192.168.1.32:8080";
-export const nginxURL = "http://192.168.1.32/uploads/";
+const dev = {
+  serverURL: "http://192.168.1.32:8080",
+  nginxURL: "http://192.168.1.32/uploads/"
+}
+
+const prod = {
+  serverURL: "http://192.168.1.218:8080",
+  nginxURL: "http://192.168.1.218/uploads/"
+}
+
+export const serverURL = prod.serverURL;
+export const nginxURL = prod.nginxURL;
